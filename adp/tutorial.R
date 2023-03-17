e4b230b7a1fe426607ee59f8c135be074940fe9b
Making TARS <43284101+arti1117@users.noreply.github.com>
2023-03-17 10:15:50 +0900

    Update tutorial.R

--- adp/tutorial.R
+++ adp/tutorial.R
@@ -1,3 +1,11 @@
-# Tutorial Page
+# This is comment
 
-A <- c(1:10)
+# Remove variables
+rm(list=ls())
+
+a<-c(1:10)
+f<-factor(c(10:11))
+f1<-factor("A", c("A", "B"))
+gender<-factor("M", c("M", "F"))
+
+comb<-stack(list(v1=c(1:5), v2=c(6:10), v3=c(11:15)))
